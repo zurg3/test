475fe7faca0cf2261fd43aa49bf9578bc788038b
zurg3 <20664125+zurg3@users.noreply.github.com>
2018-02-20 18:33:41 +0300

    Fixed tabs in calc.lua

--- calc.lua
+++ calc.lua
@@ -8,17 +8,17 @@ a = tonumber(a)
 c = tonumber(c)
 
 if b == '+' then
-	d = a + c
-	print(d)
+  d = a + c
+  print(d)
 elseif b == '-' then
-	d = a - c
-	print(d)
+  d = a - c
+  print(d)
 elseif b == '*' then
-	d = a * c
-	print(d)
+  d = a * c
+  print(d)
 elseif b == '/' then
-	d = a / c
-	print(d)
+  d = a / c
+  print(d)
 else
-	print('Your input is incorrect!')
+  print('Your input is incorrect!')
 end
