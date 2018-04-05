ac500dc3e97377b4eaa66b9f5131000a2ca9a463
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2018-04-05 18:56:02 +0300

    Updated arch_linux_install_2.sh

--- arch_linux_install_2.sh
+++ arch_linux_install_2.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-uname -r
+useradd -m -g users -G wheel -s /bin/bash zurg3
+passwd zurg3
+
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+pacman -Syy
+pacman -S xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils
+pacman -S xfce4 xfce4-goodies lxdm
+pacman -S networkmanager network-manager-applet ppp
+systemctl enable gdm NetworkManager
+
+reboot
