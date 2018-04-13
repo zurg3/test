1d67c0cc86575866abbd66e52884bbb37fa6e21c
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2018-04-13 16:51:12 +0300

    Updated arch_linux_install.sh

--- arch_linux_install.sh
+++ arch_linux_install.sh
@@ -78,23 +78,6 @@ genfstab -pU /mnt >> /mnt/etc/fstab
   echo "exit";
 ) | arch-chroot /mnt
 
-# arch-chroot /mnt
-
-# echo "zurg3" > /etc/hostname
-# ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-# echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-# echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
-# locale-gen
-# echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
-# echo "KEYMAP=ru" > /etc/vconsole.conf
-# echo "FONT=cyr-sun16" >> /etc/vconsole.conf
-# mkinitcpio -p linux
-# passwd
-# pacman -S grub
-# grub-install /dev/sda
-# grub-mkconfig -o /boot/grub/grub.cfg
-# exit
-
 echo "#!/bin/bash
 
 useradd -m -g users -G wheel -s /bin/bash $set_username
@@ -112,7 +95,7 @@ echo \"Include = /etc/pacman.d/mirrorlist\" >> /etc/pacman.conf
 
 pacman -Syy
 pacman -S xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils
-pacman -S xfce4 xfce4-goodies lxdm
+pacman -S xfce4 xfce4-goodies lxdm ttf-dejavu
 pacman -S networkmanager network-manager-applet ppp
 systemctl enable lxdm NetworkManager
 
@@ -122,5 +105,5 @@ reboot" > /mnt/root/arch_linux_setting.sh
 
 chmod +x /mnt/root/arch_linux_setting.sh
 
-umount /mnt/{boot,home,}
+umount -R /mnt
 reboot
