c8d0171032973159beae8ee54bed0fdd00a1ae38
zurg3 <20664125+zurg3@users.noreply.github.com>
2018-05-16 23:46:26 +0300

    Updated installed_packages_list_sort.txt, added calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float addition, subtraction, multiplication, division;
+  float a;
+  float b;
+  cout << "Enter the first number: ";
+  cin >> a;
+  cout << "Enter the first number: ";
+  cin >> b;
+  addition = a + b;
+  subtraction = a - b;
+  multiplication = a * b;
+  division = a / b;
+  cout << a << "+" << b << "=" << addition  << endl;
+  cout << a << "-" << b << "=" << subtraction << endl;
+  cout << a << "*" << b << "=" << multiplication  << endl;
+  cout << a << "/" << b << "=" << division  << endl;
+  return 0;
+}
