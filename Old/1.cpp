189095a69cfc2526cda759fa2d2f070614473036
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2018-05-20 18:23:11 +0300

    Edited 1.cpp

--- Old/1.cpp
+++ Old/1.cpp
@@ -1,10 +1,7 @@
-// 1.cpp
-// <zurg3><Программа печатающая текст><15:53><19.01.2018>
-
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
   cout << "My first program!" << endl;
   return 0;
