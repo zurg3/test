350ca6734018a1a5ed91c8251a0bef7057793cb6
zurg3 <20664125+zurg3@users.noreply.github.com>
2019-04-25 20:49:51 +0300

    Removed some files

--- google_play_top_apps.js
+++
@@ -1,4 +0,0 @@
-var top_len = prompt('Enter list length');
-for (var i = 0; i < top_len; i++) {
-  console.log(document.getElementsByClassName('details')[i].innerText);
-}
--- imdb_top.js
+++
@@ -1,4 +0,0 @@
-for (i = 0; i < 250; i++) {
-  var imdb_top_item = document.getElementsByClassName('titleColumn')[i].innerText;
-  console.log(imdb_top_item);
-}
