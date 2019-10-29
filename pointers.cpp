94576c5f68300a75c6b3db21b5b32a3a12bde4c5
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-10-29 23:36:58 +0300

    Added pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 33;
+  int* b = &a;
+  int c = *b;
+  
+  cout << a << " " << *&a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  
+  return 0;
+}
