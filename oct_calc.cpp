098c979238443a07f97d4c153c196c5073927842
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-10-30 16:52:14 +0300

    Added oct_calc.cpp

---
+++ oct_calc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int a, b;
+  
+  cout << "Enter the 1st num: ";
+  cin >> a;
+  
+  cout << "Enter the 2nd num: ";
+  cin >> b;
+  
+  cout << a << " + " << b << " = " << a + b << endl;
+  cout << setbase(8);
+  cout << a << " + " << b << " = " << a + b << endl;
+  
+  return 0;
+}
