d7a1eb93cbb7790bef499855d87008a93889b89b
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-02-09 15:35:38 +0300

    Added some files

---
+++ electron_app/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + "/img/icon.png",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  //win.loadURL('https://google.com')
+
+  //win.webContents.openDevTools();
+
+  win.on("closed", function () {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
---
+++ electron_app/main_js.js
@@ -0,0 +1,24 @@
+var app_version = require('electron').remote.app.getVersion();
+var app_name = 'TestElectronApp' + ' v' + app_version;
+
+function openURL_click() {
+  var get_url = document.getElementById('open_url').value;
+
+  window.open(get_url, '_self');
+}
+
+function openURL_enter() {
+  var get_url = document.getElementById('open_url').value;
+
+  if (event.key === "Enter") {
+    window.open(get_url, '_self');
+  }
+}
+
+function clicker() {
+  document.getElementById('clicker_score').innerText++;
+}
+
+document.title = app_name;
+
+document.write('<h1 align="center">' + app_name + '</h1>');
