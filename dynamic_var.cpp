7a8a5d1000c8cd4152a36cda3994bfc21ed08799
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2020-07-27 22:53:10 +0300

    Added dynamic_var.cpp

---
+++ dynamic_var.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int *a = new int;
+  int *b = new int(5);
+  int *c = new int;
+  
+  *a = 10;
+  *c = *a + *b;
+  
+  cout << *a << " + " << *b << " = " << *c << endl;
+  
+  delete c;
+  delete b;
+  delete a;
+
+  return 0;
+}
