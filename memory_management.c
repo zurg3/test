f5bf5947cd430a04a5f141468f7390ae75ae27ae
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2021-01-07 18:35:36 +0300

    Edited memory_management.c

--- memory_management.c
+++ memory_management.c
@@ -7,7 +7,7 @@ int main() {
   printf("Enter number of elements: ");
   scanf("%d", &n);
 
-  ptr = (int*) malloc(n * sizeof(int));
+  ptr = (int*)malloc(n * sizeof(int));
 
   if (ptr == NULL) {
     printf("Error! Memory not allocated!\n");
